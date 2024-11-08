d55f78aa96e22d24b8c1c72da9fb211d45462103
thebhagirathi <thebhagirathi@gmail.com>
2024-11-09 03:23:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// This function will change the header's background color
+function changeHeaderColor() {
+    document.querySelector('header').style.backgroundColor = 'blue';
+}
+
+// Adding the event listener to the button
+document.querySelector('button').addEventListener('click', changeHeaderColor);
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Hello, please learn dont waste your time!');
+});
